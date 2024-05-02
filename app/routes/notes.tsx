f1bf35a2b237983e1db6930b06045a8ca5ddafc3
jerome-shiftleft <jerome@theshiftleft.com>
2024-05-03 00:31:13 +0800

    Add Notes route and component

---
+++ app/routes/notes.tsx
@@ -0,0 +1,18 @@
+export default function Notes() {
+  return (
+    <main>
+      <h1>Notes</h1>
+      <ul>
+        <li>
+          <a href="/notes/1">Note 1</a>
+        </li>
+        <li>
+          <a href="/notes/2">Note 2</a>
+        </li>
+        <li>
+          <a href="/notes/3">Note 3</a>
+        </li>
+      </ul>
+    </main>
+  );
+}
